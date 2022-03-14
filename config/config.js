b05b7e27c79035c17921a8ba7e5a160fd6da56f5
neandermenezes <neander2801@gmail.com>
2022-03-14 18:07:50 -0300

    config changed: HOST -> HOSTNAME

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  host: process.env.MYSQL_HOST,
+  host: process.env.HOSTNAME,
   dialect: 'mysql',
 };
 
